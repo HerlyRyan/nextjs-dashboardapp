9b8c74ccc0426008b7581f1eaddcb94990544bc2
Herly Riyanto Hidayat <herlyryanth@gmail.com>
2024-10-24 15:51:43 +0800

    add many features

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,28 @@
-export default function Page() {
-    return <p>Customers Page</p>
+import CustomersTable from "@/app/ui/customers/table";
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import { Suspense } from "react";
+
+export default async function Page({
+    searchParam,
+}: {
+    searchParam?: {
+        query?: string;
+        page?: string;
+    };
+}) {
+    const query = searchParam?.query || "";
+    const currentPage = Number(searchParam?.page) || 1;
+
+    const customers = await fetchFilteredCustomers(query)
+
+    return (
+        <div className="w-full">                        
+            <Suspense key={query + currentPage} >
+                <CustomersTable customers={ customers} />
+            </Suspense>
+            <div className="mt-5 flex w-full justify-center">
+                {/* <Pagination totalPages={totalPages} /> */}
+            </div>
+        </div>
+    )
 }
